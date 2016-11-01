0d57849a24b3604a64a8a831353e0dcf1f8445db
Matthew Bond <matthew.randal.bond@gmail.com>
2016-10-31 22:09:17 -0700

    added .gitignore file and the base xcode project

---
+++ iKid/iKid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by Matt Bond on 10/31/16.
+//  Copyright © 2016 Matt Bond. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
